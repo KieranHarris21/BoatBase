ab76fc39b9e2ed32cab6a556dc61091f661b118c
tireshredder <kieranharris209@gmail.com>
2024-12-12 13:09:10 +0200

    android build working added icon and splash screen

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,7 +7,7 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Boating thing 2</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
