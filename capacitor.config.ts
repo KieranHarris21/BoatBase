004ff82b66f80303bf5a53800c3fb7cc2aeff9d7
Kieran Harris <kieranharris209@gmail.com>
2024-12-19 22:26:49 +0200

    changed the name to BoatBase

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Boating App',
+  appName: 'BoatBase',
   webDir: 'dist'
 };
 
